d4068277385cc2f85d510bff23014ba8f52c2872
cristi <cristi.draghici@coosto.com>
2019-11-02 00:52:54 +0100

    Changed dashboard activity to show the project name only the first time, when the activities are from the same one

--- Template/event/dashevents.php
+++ Template/event/dashevents.php
@@ -1,10 +1,13 @@
 <?php if (empty($events)): ?>
     <p class="alert"><?= t('There is no activity yet.') ?></p>
 <?php else: ?>
+    <?php $current_project_id = null; ?>
     <?php foreach ($events as $event): ?>
+        <?php if ($current_project_id !== $event['task']['project_id']) : ?>
         <summary class="accordion-title">
           <a href="?controller=BoardViewController&action=show&project_id=<?= $event['task']['project_id'] ?>"><?= $event['task']['project_name'] ?></a>
         </summary>
+        <?php endif; ?>
 
         <div class="activity-event">
             <?= $this->avatar->render(
@@ -20,5 +23,6 @@
             </div>
 
         </div>
+        <?php $current_project_id = $event['task']['project_id']; ?>
     <?php endforeach ?>
 <?php endif ?>
